15940080ddb0eab8f774ac7d5fa3e369ebb17621
Giovanni Lerma Suárez <giosuarezler@gmail.com>
2019-10-29 23:28:48 -0500

    Initial commit https://demonuts.com/pick-image-gallery-camera-android/

---
+++ app/src/main/java/com/example/pruebaolimpia/ui/auth/GeolocationActivity.kt
@@ -0,0 +1,13 @@
+package com.example.pruebaolimpia.ui.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.pruebaolimpia.R
+
+class GeolocationActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_geolocation)
+    }
+}
---
+++ app/src/main/java/com/example/pruebaolimpia/ui/auth/SaveActivity.kt
@@ -0,0 +1,13 @@
+package com.example.pruebaolimpia.ui.auth
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.pruebaolimpia.R
+
+class SaveActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_save)
+    }
+}
